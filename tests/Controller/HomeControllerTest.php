c4dc5e6e565b6a56f81771f89beaa87140b1f58b
Ben Merken <ben.merken@gmail.com>
2019-11-01 18:38:08 +0100

    Add security.yaml in config/packages/test for HTTP authentication and functional test for authenticated User in HomeController
    For more info on these changes, please check https://symfony.com/doc/current/testing/http_authentication.html

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -8,11 +8,42 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomeControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function userAndPasswordProvider()
+    {
+        return [
+            [
+                ['PHP_AUTH_USER' => 'lector1@pxl.be',
+                    'PHP_AUTH_PW' => 'secret']
+            ],
+            [
+                ['PHP_AUTH_USER' => 'lector4@pxl.be',
+                    'PHP_AUTH_PW' => 'secret']
+            ],
+            [
+                ['PHP_AUTH_USER' => 'lector7@pxl.be',
+                    'PHP_AUTH_PW' => 'secret']
+            ],
+        ];
+    }
+
+    public function testIndex_noUserLoggedIn()
     {
         $client = static::createClient();
         $client->request('GET', '/');
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertSelectorTextContains('h1', 'Welcome to the PXL Asset Management tool!');
+    }
+
+    /**
+     * @dataProvider userAndPasswordProvider
+     */
+    public function testIndex_UserLoggedIn($userAndPassword)
+    {
+        $client = static::createClient([], $userAndPassword);
+        $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertSelectorTextContains('h1', $userAndPassword['PHP_AUTH_USER']);
     }
 }
