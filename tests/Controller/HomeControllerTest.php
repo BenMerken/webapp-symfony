517242669277a6eef07bc75f2abbbe3406d5f301
Ben Merken <ben.merken@gmail.com>
2019-12-15 00:18:04 +0100

    Add functional tests for HomeControllerTest
    Add functional tests that assert on statuscodes and table rows

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -26,13 +26,14 @@ class HomeControllerTest extends WebTestCase
         ];
     }
 
-    public function testIndex_AnonymousUser_Statuscode200AndH1()
+    public function testIndex_AnonymousUser_Statuscode200AndH1EqualsStringLiteral()
     {
         $client = static::createClient();
-        $client->request('GET', '/');
+        $crawler = $client->request('GET', '/');
+        $h1 = $crawler->filter('h1')->text();
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertSelectorTextContains('h1', 'Welcome to the PXL Asset Management tool!');
+        $this->assertEquals('Welcome to the PXL Asset Management tool!', $h1);
     }
 
     /**
@@ -41,9 +42,53 @@ class HomeControllerTest extends WebTestCase
     public function testIndex_UserLoggedIn_Statuscode200AndH1ContainsUserEmail($userAndPassword)
     {
         $client = static::createClient([], $userAndPassword);
-        $client->request('GET', '/');
+        $crawler = $client->request('GET', '/');
+        $h1 = $crawler->filter('h1')->text();
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-        $this->assertSelectorTextContains('h1', $userAndPassword['PHP_AUTH_USER']);
+        $this->assertContains($userAndPassword['PHP_AUTH_USER'], $h1);
+    }
+
+    public function testIndex_AnonymousUserClicksOnAnchorWithTextHome_Statuscode200()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $HomeAnchorLink = $crawler->filter('a:contains("Home")')->link();
+        $client->click($HomeAnchorLink);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testIndex_AnonymousUserClicksOnAnchorWithTextLogin_Statuscode200()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $loginAnchorLink = $crawler->filter('a:contains("Login")')->link();
+        $client->click($loginAnchorLink);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testIndex_AnonymousUser_TableWithAssetsContainsSevenRowsInTableBody()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $tableBodyRowCount = $crawler->filter('tbody')->children()->count();
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(7, $tableBodyRowCount);
+    }
+
+    /**
+     * @dataProvider userAndPasswordProvider
+     */
+    public function testIndex_UserLoggedIn_TableWithAssetsContainsEightRowsInTableBody($userAndPassword)
+    {
+        $client = static::createClient([], $userAndPassword);
+        $crawler = $client->request('GET', '/');
+        $tableBodyRowCount = $crawler->filter('tbody')->children()->count();
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(8, $tableBodyRowCount);
     }
 }
